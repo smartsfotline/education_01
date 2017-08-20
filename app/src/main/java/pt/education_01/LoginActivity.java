7adb3609d917791b61a6d5f88f9a3f39d1448584
shakavets <shakavets@gmail.com>
2017-08-20 16:56:43 +0300

    3-d test commit

--- app/src/main/java/pt/education_01/LoginActivity.java
+++ app/src/main/java/pt/education_01/LoginActivity.java
@@ -1,5 +1,5 @@
 package pt.education_01;
-//shakavets test commit 02
+//shakavets test commit 03
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
